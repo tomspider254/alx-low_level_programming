3d7fabb8cda1a08fc0ed82eb6bc60524a8decebb
tomspider254 <tommwaniki002@gmail.com>
2023-04-09 23:32:13 -0700

    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,13 +4,13 @@
  * get_bit - returns the value of a bit at a given 
  * index.
  * @n: unsigned long int input.
- * @index: index of a bit.
+ * @index: index of the bit.
  *
- * Return: value of a bit
+ * Return: value of the bit
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigne int i;
+	unsigned int i;
 
 	if (n == 0 && index < 64)
 		return (0);
